c0f968d048ac807f5a5752a77b263174c881b74c
Gabriel José <gabrieljose150509@gmail.com>
2026-04-22 21:52:22 -0300

    Add: implementação do menu interativo

---
+++ primeiro_projeto.py
@@ -0,0 +1,11 @@
+def menu():
+    print("="*30)
+    print("SISTEMA DE CADASTRO")
+    print("="*30)
+    print("1 - Adicionar Usuário")
+    print("2 - Listar Usuários")
+    print("3 - Sair")
+    print("="*30)
+
+if __name__ == "__main__":
+    menu()
